12601cb79d88321dc9e1b8eeb3dd1f8914eb8f53
DGreegman <obeagug@gmail.com>
2025-02-14 12:39:20 +0100

    Still on cors issues

--- dist/index.js
+++ dist/index.js
@@ -13,20 +13,18 @@ const cors_1 = __importDefault(require("cors"));
 const attached_email_route_1 = __importDefault(require("./routes/attached-email.route"));
 const global_error_1 = require("./errors/global.error");
 const bulk_admin_route_1 = __importDefault(require("./routes/bulk-admin.route"));
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
 (0, dotenv_1.configDotenv)();
 (0, db_1.default)();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-/* const corsOptions = {
-    origin: 'https://etsapsfrica.com',
+const corsOptions = {
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'authorization', 'Authorization']
-}; */
-app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)());
+};
+app.use((0, cors_1.default)(corsOptions));
 app.use('/api/v1/auth', user_route_1.default);
 app.use('/api', payment_route_1.default);
 app.use('/api/v1/attachment', attached_email_route_1.default);
--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,13 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/* const corsOptions = {
-    origin: 'https://etsapsfrica.com',
+const corsOptions = {
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'authorization', 'Authorization']
-}; */
-app.use(cookieParser());
-app.use(cors());
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api/v1/auth', router);
 app.use('/api', paymentRouter);
